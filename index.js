006cec6fc1e5e65e9b56c7c28e3f0770c2a6ad70
aoki <aoki@ferix.jp>
2019-05-02 23:43:29 +0900

    イベントリスナ登録

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ console.log("start");
 window.onload = () => {
   const button = document.getElementById("insert-button");
   console.log(button);
+  button.addEventListener("click", (event) => {
+    // ボタン押下時の処理
+    console.log("click!!");
+  });
 }
